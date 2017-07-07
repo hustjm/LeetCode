49ffb4dad499ae49092b4a90dfbc295630d777f0
hustjm <hust_jiangmian@foxmail.com>
2017-07-07 14:44:09 +0800

    1）test ssh_key 2) solution frame to problem media_of_two_sorted_arrays

---
+++ leetcode/src/solutions/media_of_two_sorted_arrays/media_of_two_sorted_arrays.cpp
@@ -0,0 +1,14 @@
+#include "media_of_two_sorted_arrays.h"
+
+
+double SLT_MediaOfTwoSortedArrays::findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+{
+    double ret = 0.0;
+
+    return ret;
+}
+
+void TestCase_MediaOfTwoSortedArrays::run()
+{
+
+}
---
+++ leetcode/src/solutions/media_of_two_sorted_arrays/media_of_two_sorted_arrays.h
@@ -0,0 +1,39 @@
+/*********************************************************************
+ * Created:       2017/07/07
+ * File name:     media_of_two_sorted_arrays.h
+ * Author:        JiangMian
+ * Revision:      1.0.0
+ *
+ * Copyright (C)  2017 - 2020 , All Rights Reserved.
+ *
+ * Purpose: 
+ *********************************************************************/
+
+#ifndef __MEDIA_OF_TWO_SORTED_ARRAYS_H_2017_07_07__
+#define __MEDIA_OF_TWO_SORTED_ARRAYS_H_2017_07_07__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class SLT_MediaOfTwoSortedArrays
+{
+public:
+    SLT_MediaOfTwoSortedArrays() {};
+    virtual ~SLT_MediaOfTwoSortedArrays() {};
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
+};
+
+class TestCase_MediaOfTwoSortedArrays
+{
+public:
+    TestCase_MediaOfTwoSortedArrays() {};
+    virtual ~TestCase_MediaOfTwoSortedArrays() {};
+public:
+    void run();
+};
+
+
+#endif //end ifndef  __MEDIA_OF_TWO_SORTED_ARRAYS_H_2017_07_07__
